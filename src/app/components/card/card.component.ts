7ebd21040fb740ea807e3b8bc733033569d0da23
D2Cabral <diegomirks@icloud.com>
2023-05-11 16:46:51 -0300

    Search pokemons

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -7,13 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CardComponent implements OnInit {
-  name:string = "Bulbasaur"
-  attributsTypes:string[] = ['Fire', 'Rock']
-  constructor(){}
+  pokemon?:PokemonData | any
 
+
+  constructor(
+    private service: PokemonService)
+    { //injetando dependencia do service
+      this.pokemon = {
+        id: 0,
+        name: '',
+        sprites: {
+          front_dafault:''
+         },
+        types:[]
+      }
+
+    }
 
   ngOnInit(): void {
 
+    this.getPokemon('pikachu')
+
   }
+
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe({
+
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types
+
+        }
+     }
+    })
+
+  }
+
 
 }
---
+++ src/app/models/pokemonData.ts
@@ -0,0 +1,16 @@
+export type PokemonData = {
+  name:string
+  id:number
+  sprites:{
+    front_dafault:string
+  }
+  types:{
+    slot:Number
+    type:{
+      name:String
+      url:String
+  }
+  }[]
+
+}
+
